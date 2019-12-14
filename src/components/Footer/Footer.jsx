8f78c93b6e9ba3f6e0d66cf0fafc84da378afaaf
raisoturu <anvaqta@student.telkomuniversity.ac.id>
2019-12-14 22:25:24 +0700

    add navbar & footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,115 @@
 import React from 'react'
-
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import style from './Footer.module.css'
+import cx from 'classnames'
+import Container from 'react-bootstrap/Container'
 const Footer = () => {
   return (
-    <div>
-      <h6 style={{ color: 'blue' }}> THIS IS FOOTER</h6>
-    </div>
+    <Container fluid className={cx('mt-md-5', style.footer)}>
+      <Row>
+        <Col md="4">
+          <img
+            src="/assets/img/logo-alt.png"
+            alt="logo openbook"
+            className={style.logo}
+          />
+          <p className="mt-2 text-justify">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus
+            odio omnis pariatur, velit repellat beatae dolore necessitatibus
+            veniam culpa ipsum? Dolor fugiat repudiandae, necessitatibus ducimus
+            maxime autem consectetur quidem ullam.
+          </p>
+        </Col>
+        <Col md="2" className="pl-md-5">
+          <p className={style.title}>Tim open book</p>
+          <div className="my-1">
+            <a href="#link" className={style.link}>
+              Tentang kami
+            </a>
+          </div>
+          <div className="my-1">
+            <a href="#link" className={style.link}>
+              Kerjasama
+            </a>
+          </div>
+          <div className="my-1">
+            <a href="#link" className={style.link}>
+              Bantuan
+            </a>
+          </div>
+          <div className="my-1">
+            <a href="#link" className={style.link}>
+              Kebijakan privasi
+            </a>
+          </div>
+        </Col>
+        <Col md="3">
+          <p className={style.title}>Ketringan</p>
+          <div className="d-flex align-items-center mb-3">
+            <img src="/assets/img/icons/email.svg" alt="email" />
+            <a
+              href="mailto:mantap@gmail.com"
+              className={cx('ml-2', style.link)}
+            >
+              mantap@gmail.com
+            </a>
+          </div>
+          <div className="d-flex align-items-center mb-3">
+            <img src="/assets/img/icons/wa.svg" alt="email" />
+            <a
+              href="mailto:mantap@gmail.com"
+              className={cx('ml-2', style.link)}
+            >
+              81923801982301
+            </a>
+          </div>
+        </Col>
+        <Col md="3">
+          <p className={style.title}>Temukan kami</p>
+          <div>
+            <img
+              className={style['social-icon']}
+              src="/assets/img/icons/fb.svg"
+              alt="fb"
+            />
+            <img
+              className={style['social-icon']}
+              src="/assets/img/icons/twitter.svg"
+              alt="twitter"
+            />
+            <img
+              className={style['social-icon']}
+              src="/assets/img/icons/linkedin.svg"
+              alt="linkedin"
+            />
+            <img
+              className={style['social-icon']}
+              src="/assets/img/icons/ig.svg"
+              alt="ig"
+            />
+          </div>
+          <Row className="mt-3">
+            <Col sm="1" className="d-none d-sm-block">
+              <img src="/assets/img/icons/location.svg" alt="location" />
+            </Col>
+            <Col>
+              <p>
+                Jl. Telekomunikasi, Sukapura Kec. Dayeuhkolot, Bandung, Jawa
+                Barat 40257 Indonesia
+              </p>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="mt-5">
+        <Col className="text-center">
+          <p className="mb-1">
+            Made with love by raisoturu © 2019. All Right Reserved
+          </p>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import BSNavbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { NavLink as Link } from 'react-router-dom'
 import style from './Navbar.module.css'
+import cx from 'classnames'
 const Navbar = () => {
   return (
     <div>
-      <h6 className={style.something}>this should be navbar</h6>
+      <BSNavbar variant="light" className={cx(style.nav, 'shadow-sm px-md-3')}>
+        <Link to="/">
+          <img
+            src="/assets/img/logo.png"
+            alt="openbook"
+            className="navbar-brand"
+            height="45"
+          />
+        </Link>
+        <Form inline className="ml-auto">
+          <div className={cx(style.searchBox, 'mr-2')}>
+            <Form.Control placeholder="Cari buku..." className="border-0" />
+            <Link to="/search" className={style.searchIcon}>
+              <FontAwesomeIcon icon={faSearch} />
+            </Link>
+          </div>
+        </Form>
+        <Nav>
+          <Link className="nav-link mr-1" to="/about">
+            Tentang
+          </Link>
+          <Link className="nav-link mr-1" to="/login">
+            Masuk
+          </Link>
+          <Link
+            className={cx(style.daftarBtn, 'nav-link btn btn-primary')}
+            to="/register"
+          >
+            Daftar
+          </Link>
+        </Nav>
+      </BSNavbar>
     </div>
   )
 }
